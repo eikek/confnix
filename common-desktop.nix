0317019fcde3643c105fb7b4950422141884bd99
Eike Kettner <eike.kettner@posteo.de>
2015-01-18 00:13:35 +0100

    allow user to mount cifs drives

--- common-desktop.nix
+++ common-desktop.nix
@@ -14,12 +14,15 @@
     cpuFreqGovernor = "ondemand";
   };
 
+  # needed for the `user` option below
+  security.setuidPrograms = [ "mount.cifs" ];
+
   fileSystems = builtins.listToAttrs (map (mp:
     { name = "/mnt/nas/" + mp;
       value = {
         device = "//nas/" + mp;
         fsType = "cifs";
-        options = "noauto,username=eike,password=eike,uid=1000,gid=100";
+        options = "noauto,user,username=eike,password=eike,uid=1000,gid=100";
         noCheck = true;
       };
     }) ["backups" "dokumente" "downloads" "home" "music" "photo" "safe" "video"]);
