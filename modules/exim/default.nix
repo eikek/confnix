770a51638c15ba0d4fa1aee32c2b7dca664193d8
Eike Kettner <eike.kettner@posteo.de>
2015-11-21 13:54:49 +0100

    fixup for bumping exim

--- modules/exim/default.nix
+++ modules/exim/default.nix
@@ -6,7 +6,7 @@ let
   cfg = config.services.exim;
   user = "exim";
   group = "exim";
-  version = "4.85";
+  version = "4.86";
   eximConfig = ''
     primary_hostname = ${cfg.primaryHostname}
     domainlist local_domains = ${concatStringsSep ":" cfg.localDomains}
