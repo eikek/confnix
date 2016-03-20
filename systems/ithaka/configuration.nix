7490f77ed4c831a1ebc6e1714bd1da505d9360a7
Eike Kettner <eike.kettner@posteo.de>
2016-03-20 01:23:21 +0100

    ithaka: add kvm host network settings

--- systems/ithaka/configuration.nix
+++ systems/ithaka/configuration.nix
@@ -24,6 +24,10 @@ let mykey = builtins.readFile /home/eike/.ssh/id_rsa.pub; in
     dpi = 140;
   };
 
+  users.groups.kvm = {
+    members = [ "eike" ];
+  };
+
   networking = {
     hostName = "ithaka";
     wireless = {
@@ -37,6 +41,14 @@ let mykey = builtins.readFile /home/eike/.ssh/id_rsa.pub; in
       externalInterface = "enp5s0";
       internalInterfaces = [ "ve-+" ];
     };
+
+   localCommands = ''
+     ${pkgs.vde2}/bin/vde_switch -tap tap0 -mod 660 -group kvm -daemon
+     ip addr add 10.0.2.1/24 dev tap0
+     ip link set dev tap0 up
+     ${pkgs.procps}/sbin/sysctl -w net.ipv4.ip_forward=1
+     ${pkgs.iptables}/sbin/iptables -t nat -A POSTROUTING -s 10.0.2.0/24 -j MASQUERADE
+   '';
   };
 
   services.xserver = {
