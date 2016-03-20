b2e1b5501c67ac1b4a41579436321d6cff538b2d
Eike Kettner <eike.kettner@posteo.de>
2016-03-20 01:56:41 +0100

    add new system (vm)

---
+++ systems/macnix/configuration.nix
@@ -0,0 +1,38 @@
+{ config, pkgs, ... }:
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hw-macnix.nix
+      ../../common-desktop.nix
+    ];
+
+  boot = {
+    loader.grub = {
+      enable = true;
+      version = 2;
+      devices = [ "/dev/sda" ];
+    };
+  };
+
+  fonts.fontconfig = {
+#    dpi = 120;
+  };
+
+  networking = {
+    hostName = "macnix";
+    wireless = {
+      enable = false;
+    };
+    useDHCP = true;
+    wicd.enable = false;
+  };
+
+  environment.pathsToLink = [ "/" ];
+
+  hardware = {
+    enableAllFirmware = true;
+    bluetooth.enable = false;
+    cpu.intel.updateMicrocode = true;  #needs unfree
+    opengl.driSupport32Bit = true;
+  };
+}
