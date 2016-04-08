4a934dd6df57bd9fee55b540ab0bb18ceff51fda
Eike Kettner <eike.kettner@posteo.de>
2016-04-08 09:16:20 +0200

    macnix: more hosts entries

--- systems/macnix/configuration.nix
+++ systems/macnix/configuration.nix
@@ -19,7 +19,7 @@
   services.xserver = {
     displayManager = {
       sessionCommands = ''
-        ${pkgs.xlibs.xrandr}/bin/xrandr --output VGA-1 --mode 1440x900 --pos 0x0 --output VGA-0 --mode 1920x1200 --pos 1440x0
+#        ${pkgs.xlibs.xrandr}/bin/xrandr --output VGA-1 --mode 1440x900 --pos 0x0 --output VGA-0 --mode 1920x1200 --pos 1440x0
       '';
     };
   };
@@ -39,8 +39,15 @@
 
       # https://intranet/x/HIGkAQ
       192.168.13.27 larnags.int
+      192.168.13.28 larnags.int.backend
+      192.168.13.73 larnags.hii
       192.168.13.75 larnags.sta larnags.sta1
       192.168.13.76 larnags.sta2
+      192.168.13.77 larnags.sta.backend larnags.sta.backend1
+      192.168.13.78 larnags.sta.backend2
+      192.168.13.79 larnags.sta.dbmaster
+      192.168.13.80 larnags.sta.dbslave1
+      192.168.13.81 larnags.sta.dbslave2
     '';
     hostName = "bluecare-n46_1";
     wireless = {
