04c3b2dcd8981b6b7953c7ad1219139daa1bad67
Eike Kettner <eike.kettner@posteo.de>
2017-03-22 23:14:49 +0100

    fixed skyros testvm
    more memory, don't resize disk (not always supported)

--- systems/skyros/test/testvm.nix
+++ systems/skyros/test/testvm.nix
@@ -15,13 +15,13 @@
         ./testconf.nix
       ];
 
-      settings.primaryIp = pkgs.lib.mkForce "192.168.56.103";
+      settings.primaryIp = pkgs.lib.mkForce "192.168.56.101";
       deployment.targetEnv = "virtualbox";
-      deployment.virtualbox.memorySize = 2560;
+      deployment.virtualbox.memorySize = 3072;
       deployment.virtualbox.headless = true;
-      deployment.virtualbox.disks.disk1 = {
-        size = 20480;
-      };
+      # deployment.virtualbox.disks.disk1 = {
+      #   size = 20480;
+      # };
 
       users.extraUsers.root = {
         openssh.authorizedKeys.keys = [ mykey ];
