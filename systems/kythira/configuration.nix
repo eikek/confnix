c52c7df10f340a1045d9befeff21d44b90df3133
Eike Kettner <eike.kettner@posteo.de>
2017-03-30 23:55:51 +0200

    kythira: detect displays

--- systems/kythira/configuration.nix
+++ systems/kythira/configuration.nix
@@ -35,19 +35,19 @@ let mykey = builtins.readFile /home/eike/.ssh/id_rsa.pub; in
     useDHCP = true;
     wicd.enable = true;
 
-   #  nat = {
-   #    enable = true;
-   #    externalInterface = "enp109s0f1";
-   #    internalInterfaces = [ "ve-+" ];
-   #  };
+    nat = {
+      enable = true;
+      externalInterface = "enp109s0f1";
+      internalInterfaces = [ "ve-+" ];
+    };
 
-   # localCommands = ''
-   #   ${pkgs.vde2}/bin/vde_switch -tap tap0 -mod 660 -group kvm -daemon
-   #   ip addr add 10.0.2.1/24 dev tap0
-   #   ip link set dev tap0 up
-   #   ${pkgs.procps}/sbin/sysctl -w net.ipv4.ip_forward=1
-   #   ${pkgs.iptables}/sbin/iptables -t nat -A POSTROUTING -s 10.0.2.0/24 -j MASQUERADE
-   # '';
+   localCommands = ''
+     ${pkgs.vde2}/bin/vde_switch -tap tap0 -mod 660 -group kvm -daemon
+     ip addr add 10.0.2.1/24 dev tap0
+     ip link set dev tap0 up
+     ${pkgs.procps}/sbin/sysctl -w net.ipv4.ip_forward=1
+     ${pkgs.iptables}/sbin/iptables -t nat -A POSTROUTING -s 10.0.2.0/24 -j MASQUERADE
+   '';
   };
 
   services.xserver = {
@@ -62,6 +62,9 @@ let mykey = builtins.readFile /home/eike/.ssh/id_rsa.pub; in
     displayManager = {
       sessionCommands = ''
         xrandr --dpi 140
+        if [ $(xrandr --listmonitors | grep "^ .*3840/.*x2160/.*" | wc -l) -eq 2 ]; then
+          xrandr --output DP-0 --off
+        fi
       '';
     };
   };
