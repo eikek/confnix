cd59692e70333f6e99fbf1b904c5bfac722a27cc
Eike Kettner <eike.kettner@posteo.de>
2017-06-08 09:13:36 +0200

    bc46: update to kernel 4.11

--- systems/bc46/configuration.nix
+++ systems/bc46/configuration.nix
@@ -31,7 +31,7 @@ in
     initrd.luks.devices = [
       { device = "/dev/sda4"; name = "crootfs"; preLVM = true; }
     ];
-    kernelPackages = pkgs.linuxPackages_4_10;
+    kernelPackages = pkgs.linuxPackages_4_11;
     cleanTmpDir = true;
   };
 
