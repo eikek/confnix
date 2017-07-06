77a3b742ef5043513dc4395827aa5c944af0ea7e
Eike Kettner <eike.kettner@posteo.de>
2017-07-06 20:48:58 +0200

    update makemkv

--- pkgs/makemkv/default.nix
+++ pkgs/makemkv/default.nix
@@ -1,10 +1,11 @@
 { fetchurl, lib, makemkv }:
 
 # test key is T-Rj9dmR37zXFQlCz2ljQzy8tKHPasVYXIyREZwfDSi8uwRQjo1xw7Kzdr2iDGL@4QoW
+#             T-QWvg95pFZPQjcwoog2PxbrAlj1Ml279L3GogBfgVENxFW6fMTGgrW@RPN6aPAVH31O
 # from: http://www.makemkv.com/forum2/viewtopic.php?f=5&t=1053
 
 let
-  version = "1.10.2";
+  version = "1.10.6";
 in
 lib.overrideDerivation makemkv (attrs: {
   name = "makemkv-${version}";
@@ -13,11 +14,11 @@ lib.overrideDerivation makemkv (attrs: {
 
   src_bin = fetchurl {
     url = "http://www.makemkv.com/download/makemkv-bin-${version}.tar.gz";
-    sha256 = "0dqlpf5n9lbc3lp6h8f7gzysq7h51h4ck5bjvcwrm9qkyrlaypxp";
+    sha256 = "1qy3zbp8qmw717ni263f3ycba4p3rccqwqz6vkia9jb60578xmlr";
   };
 
   src_oss = fetchurl {
     url = "http://www.makemkv.com/download/makemkv-oss-${version}.tar.gz";
-    sha256 = "1bb57kc3l1m9naqh12n4vzkg81svyglbwzh6i2idad7r0dv5kkpb";
+    sha256 = "0dfjg3yj6z723lp77x1fvrwm6mggr6976skfnycxiwk4afh8n0mb";
   };
 })
