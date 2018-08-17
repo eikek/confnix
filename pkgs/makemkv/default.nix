e346f4303f17982df943fd617aa2c340a353cb41
Eike Kettner <eike.kettner@posteo.de>
2018-08-17 19:32:05 +0200

    Update makemkv

--- pkgs/makemkv/default.nix
+++ pkgs/makemkv/default.nix
@@ -1,6 +1,7 @@
 { fetchurl, lib, makemkv }:
 
-# test key is T-NFjyvOeJ_y4pyiTEUKgI5bQMpms@HwygOuilHIWNV6_l3Z3su9psCLBHraoFktxO4O
+# test key is T-aOOAOSdZWjJvlEAkqTMx@FmIUDQ9Uw5gNT2t0HYlnR3M7MpMj4EcvPPV3rImuagWuo
+#             T-NFjyvOeJ_y4pyiTEUKgI5bQMpms@HwygOuilHIWNV6_l3Z3su9psCLBHraoFktxO4O
 #             T-97pzDZ1bt6gLQbt9KpzffjEI0pRF_MjHnzDHBI@nwQIQpFmCmzpTlyzHfbI1ghXsR7
 #             T-Rj9dmR37zXFQlCz2ljQzy8tKHPasVYXIyREZwfDSi8uwRQjo1xw7Kzdr2iDGL@4QoW
 # from: http://www.makemkv.com/forum2/viewtopic.php?f=5&t=1053
@@ -8,7 +9,7 @@
 # if optical drive is not recognized try `modprobe sg` before starting makemkv
 
 let
-  version = "1.12.0";
+  version = "1.12.3";
 in
 lib.overrideDerivation makemkv (attrs: {
   name = "makemkv-${version}";
@@ -17,11 +18,11 @@ lib.overrideDerivation makemkv (attrs: {
 
   src_bin = fetchurl {
     url = "http://www.makemkv.com/download/makemkv-bin-${version}.tar.gz";
-    sha256 = "167l6d3hxnms4cbkhb2yx14r6119k2w4rlm9kriwxj1mz1z1q6vd";
+    sha256 = "0rggpzp7gp4y6gxnhl4saxpdwnaivwkildpwbjjh7zvmgka3749a";
   };
 
   src_oss = fetchurl {
     url = "http://www.makemkv.com/download/makemkv-oss-${version}.tar.gz";
-    sha256 = "1cz2mmz4s1fl3xfsqw1qzx7fwhgwz13sy7hjm4kbfxpvbjsy5v8q";
+    sha256 = "1w0l2rq9gyzli5ilw82v27d8v7fmchc1wdzcq06q1bsm9wmnbx1r";
   };
 })
