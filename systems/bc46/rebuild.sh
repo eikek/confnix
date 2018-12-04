5783fdb4bbfb747aa9a6c3cac9b82c873a486fa8
Eike Kettner <eike.kettner@posteo.de>
2018-12-04 08:56:45 +0100

    bc46: use correct password

--- systems/bc46/rebuild.sh
+++ systems/bc46/rebuild.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
+if [[ -z "$SUDO_USER" ]]; then
+    echo "Must run via sudo."
+    exit 1
+fi
+
+su $SUDO_USER -c "pass show bluecare/login | head -n1" > ~root/.password
 nixos-rebuild -I serverpass=~root/.password \
               -I hinpass=~root/.hinpass \
               -I nixos1703=https://github.com/NixOS/nixpkgs/archive/17.03.tar.gz \
