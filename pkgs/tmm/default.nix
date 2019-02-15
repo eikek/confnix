9e51ad9fd3886835ed1dc6f5769c841b9b7f7822
Eike Kettner <eike.kettner@posteo.de>
2019-02-15 17:31:22 +0100

    Fixing tmm package and a missing attribute

--- pkgs/tmm/default.nix
+++ pkgs/tmm/default.nix
@@ -1,5 +1,19 @@
-{stdenv, fetchurl, jre, libmediainfo, libzen, coreutils }:
+{stdenv, fetchurl, jre, libmediainfo, libzen, coreutils-full }:
+let
+  wrapper = script: ''
+    #!/usr/bin/env bash
 
+    mkdir -p ~/.tmm
+    for f in $out/program/*; do
+      ln -nsf "\$f" "\$HOME/.tmm/\$(basename \$f)"
+    done
+
+    export PATH="${jre}/bin:${coreutils-full}/bin:\$PATH"
+    export LD_LIBRARY_PATH="${libzen}/lib:${libmediainfo}/lib:\$LD_LIBRARY_PATH"
+    cd \$HOME/.tmm
+    ${script}
+  '';
+in
 stdenv.mkDerivation rec {
   version = "2.9.15_69b6104";
   name = "tinymediamanager-${version}";
@@ -21,19 +35,13 @@ stdenv.mkDerivation rec {
     mkdir -p $out/{bin,program}
     cp -R tmm-unpack/* $out/program/
     cat > $out/bin/tinyMediaManager <<-EOF
-    #!/usr/bin/env bash
-
-    mkdir -p ~/.tmm
-    for f in $out/program/*; do
-      ln -nsf "\$f" "\$HOME/.tmm/\$(basename \$f)"
-    done
-
-    export PATH="${jre}/bin:${coreutils}/bin:\$PATH"
-    export LD_LIBRARY_PATH="${libzen}/lib:${libmediainfo}/lib:\$LD_LIBRARY_PATH"
-    cd \$HOME/.tmm
-    ./tinyMediaManager.sh
+    ${wrapper "./tinyMediaManager.sh"}
     EOF
-    chmod 755 $out/bin/tinyMediaManager
+    cat > $out/bin/tinyMediaManagerCMD <<-EOF
+    ${wrapper "./tinyMediaManagerCMD.sh \\$@"}
+    EOF
+    chmod 755 $out/bin/tinyMediaManager*
+    ln -s tinyMediaManagerCMD $out/bin/tmm
   '';
 
   meta = {
@@ -41,6 +49,6 @@ stdenv.mkDerivation rec {
       tinyMediaManager is a full featured media manager to organize and clean up your media library.
     '';
     homepage = http://www.tinymediamanager.org;
-    license = stdenv.lib.licenses.apl2;
+    license = stdenv.lib.licenses.asl20;
   };
 }
