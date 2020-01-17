39344ce5fe3b79f35a6be7870100afaa9a33782c
Eike Kettner <eike.kettner@posteo.de>
2020-01-17 15:12:32 +0100

    Go back to older jdk due to X related issues
    I see a NPE when initializing awt color classes. Can be reproduced
    with this code:
    
    ```java
    import java.awt.*;
    import java.awt.color.*;
    
    public class Test {
        public static void main(String[] args) {
            ColorSpace awtColorSpace = ColorSpace.getInstance(ColorSpace.CS_sRGB);
            awtColorSpace.toRGB(new float[] { 0, 0, 0, 0 });
        }
    }
    ```
    The NPE is deep down in the `ICC_Profile` class. It works with the
    older version.

--- modules/java.nix
+++ modules/java.nix
@@ -1,14 +1,20 @@
 { config, pkgs, ... }:
-
+let
+  nixpkgs1903 = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.03.tar.gz";
+    sha256 = "1b3h4mwpi10blzpvgsc0191k4shaw3nw0qd2p82hygbr8vv4g9dv";
+  };
+  pkgs1903 = import nixpkgs1903 { config = config; };
+in
 {
   environment = {
     systemPackages = [
-      pkgs.openjdk8
+      pkgs1903.openjdk8
     ];
 
     variables = {
-      JAVA_HOME = "${pkgs.openjdk8}/lib/openjdk";
-      JDK_HOME = "${pkgs.openjdk8}/lib/openjdk";
+      JAVA_HOME = "${pkgs1903.openjdk8}/lib/openjdk";
+      JDK_HOME = "${pkgs1903.openjdk8}/lib/openjdk";
       JDK11_HOME = "${pkgs.openjdk11}/lib/openjdk";
     };
   };
