dc8661c43225c7dd2e0d973ed294d140b6c3d23d
Eike Kettner <eike.kettner@posteo.de>
2020-02-29 17:14:24 +0100

    Add more old package sets

--- nixversions.nix
+++ nixversions.nix
@@ -1,5 +1,15 @@
 config:
 let
+  nixpkgs1803 = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.03.tar.gz";
+    sha256 = "0d9pkbax0phh392j6pzkn365wbsgd0h1cmm58rwq8zf9lb0pgkg2";
+  };
+  pkgs1803 = import nixpkgs1803 { config = config; };
+  nixpkgs1809 = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.09.tar.gz";
+    sha256 = "16j95q58kkc69lfgpjkj76gw5sx8rcxwi3civm0mlfaxxyw9gzp6";
+  };
+  pkgs1809 = import nixpkgs1809 { config = config; };
   nixpkgs1903 = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.03.tar.gz";
     sha256 = "1b3h4mwpi10blzpvgsc0191k4shaw3nw0qd2p82hygbr8vv4g9dv";
@@ -12,6 +22,6 @@ let
   pkgs1909 = import nixpkgs1909 { config = config; };
 in
 {
-  inherit pkgs1903 pkgs1909;
+  inherit pkgs1803 pkgs1809 pkgs1903 pkgs1909;
 
 }
