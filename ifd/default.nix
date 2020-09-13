e7286a209b1ed54f5ae6e54ab184919f1f8bde3a
Eike Kettner <eike.kettner@posteo.de>
2020-09-13 19:49:16 +0200

    Update docspell and sharry

--- ifd/default.nix
+++ ifd/default.nix
@@ -1,13 +1,13 @@
 let
   docspellsrc = builtins.fetchTarball {
-    url = "https://github.com/eikek/docspell/archive/30d5abddd8d40cf8f4ee2fceecaa7eb4de0ed3ed.tar.gz";
-    sha256 = "0wnfrcy48vh3h7g8fp7gif4nc34n5mnwnr7i6614mi0wxy03sbvh";
+    url = "https://github.com/eikek/docspell/archive/5809f94d6f646bb88b45a3e44cad471077ee9bde.tar.gz";
+    sha256 = "1knljdji1xs0cbcf81c4dvpfs4l7kia22028kk1dk28yx1hzw67r";
   };
   docspell = import "${docspellsrc}/nix/release.nix";
 
   sharrysrc = builtins.fetchTarball {
-    url = "https://github.com/eikek/sharry/archive/2da6b9ae78064d86c743fcbdb95daf08ccf675ef.tar.gz";
-    sha256 = "081idn9m0xhx22ii1nwipybfsh28wgpnp2dknqqrmhjpw68mwc4y";
+    url = "https://github.com/eikek/sharry/archive/b569d6926729e4486983780bf7ef4a7fc3be3cf2.tar.gz";
+    sha256 = "0g10ik9hf1c44rlgn3lda0xhacmr45g538pj3zzbvdmph6f4hwi1";
   };
   sharry = import "${sharrysrc}/nix/release.nix";
 
