3af80cf8bc7eebe7fe1cde3d6b226d719e039ec2
Eike Kettner <eike.kettner@posteo.de>
2021-04-29 11:50:31 +0200

    Add openshift to n76

--- machines/n76/configuration.nix
+++ machines/n76/configuration.nix
@@ -69,6 +69,7 @@ in
     mongodb-tools
     nodePackages.grunt-cli
     nodePackages.gulp
+    openshift
     slack
     vagrant
     yarn
@@ -218,6 +219,6 @@ in
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = "19.09"; # Did you read the comment?
+  system.stateVersion = "20.09"; # Did you read the comment?
 
 }
