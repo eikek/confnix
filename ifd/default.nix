1e8100cf7c362f43a8f5b0cf7ebe8e53776d0c41
eikek <eike.kettner@posteo.de>
2022-06-18 22:46:55 +0200

    Update docspell, sharry

--- ifd/default.nix
+++ ifd/default.nix
@@ -1,7 +1,7 @@
 let
   docspellsrc = builtins.fetchTarball {
-    url = "https://github.com/eikek/docspell/archive/f9f65044719f477717c508ecc2f511812441d63b.tar.gz";
-    sha256 = "15cawh7bjxjyrc0hnw15kzdgqf7nvmlr452ppkb6xblx02zng4v7";
+    url = "https://github.com/eikek/docspell/archive/c6a9a17f896a1680a579ea83c4af06c5a7a79e4d.tar.gz";
+    sha256 = "sha256:014sabvw0vdyrjbmanpw22f3wwrm8h7k8fh1hcvdn3iln6s71b2x";
   };
   docspell = import "${docspellsrc}/nix/release.nix";
 
@@ -18,8 +18,8 @@ let
   ds4e = import "${ds4esrc}/nix/ds4e.nix";
 
   sharrysrc = builtins.fetchTarball {
-    url = "https://github.com/eikek/sharry/archive/f984c34bd03ac844b9792b0338747ee632c93512.tar.gz";
-    sha256 = "0062hzg27mzdbd69nxwjvrpaa6286gv39gh2mnpazak73bllgc6m";
+    url = "https://github.com/eikek/sharry/archive/f0836051b4d656aa185dcb37d7daf842b6425ee8.tar.gz";
+    sha256 = "1gm8nyvib9p22s23b9fh2ni15zh2j2xi62p8as2v936pjdhw5ks9";
   };
   sharry = import "${sharrysrc}/nix/release.nix";
 
