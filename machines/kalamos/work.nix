b87e503348e761fdef01e71833c25aed0084c134
eikek <eike.kettner@posteo.de>
2023-05-18 10:36:32 +0200

    Package updates

--- machines/kalamos/work.nix
+++ machines/kalamos/work.nix
@@ -28,6 +28,7 @@ in
         kubectl
         kubernetes
         openjdk17
+        sops
       ];
   };
 
--- modules/java.nix
+++ modules/java.nix
@@ -3,17 +3,25 @@ let
   p = import ../nixversions.nix config;
 in
 {
+
+  services.bloop = {
+    install = true;
+  };
+
+  programs.java.package = pkgs.openjdk;
+
   environment = {
     systemPackages = [
-      pkgs.openjdk11
+      pkgs.openjdk
     ];
 
     variables = {
       #JAVA8_HOME = "${p.pkgs1903.openjdk8}/lib/openjdk";
-      JAVA_HOME = "${pkgs.openjdk11}/lib/openjdk";
-      JDK_HOME = "${pkgs.openjdk11}/lib/openjdk";
+      JAVA_HOME = "${pkgs.openjdk}/lib/openjdk";
+      JDK_HOME = "${pkgs.openjdk}/lib/openjdk";
       JDK11_HOME = "${pkgs.openjdk11}/lib/openjdk";
       JDK17_HOME = "${pkgs.openjdk17}/lib/openjdk";
+      #JDK_UNSTABLE_HOME = "${p.pkgsUnstable.openjdk}/lib/openjdk";
     };
   };
 
