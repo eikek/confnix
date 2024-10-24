d692bb29419a77153bbdf1720337d42de37daa98
eikek <eike.kettner@posteo.de>
2024-10-24 11:03:44 +0200

    more locales

--- modules/region-neo.nix
+++ modules/region-neo.nix
@@ -4,6 +4,13 @@
   console.keyMap = "neo";
   i18n = {
     defaultLocale = "en_GB.UTF-8";
+
+    supportedLocales = [
+      "C.UTF-8/UTF-8"
+      "en_US.UTF-8/UTF-8"
+      "de_DE.UTF-8/UTF-8"
+      "de_CH.UTF-8/UTF-8"
+    ];
   };
 
   # Set your time zone.
