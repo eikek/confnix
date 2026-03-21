d362bc49a4135ba28e07e47b5448e2ea912abcf9
eikek <eike.kettner@posteo.de>
2026-03-21 14:55:54 +0100

    Add wireguard peer

--- machines/icaria/vpn.nix
+++ machines/icaria/vpn.nix
@@ -8,4 +8,22 @@
     };
   };
 
+  networking.wg-quick.interfaces = {
+    wg0 = {
+      autostart = false;
+      address = [ "10.100.0.3/32"  ];
+      #dns = [ "10.100.0.1" ];
+      privateKeyFile = "/root/wireguard/myself.key";
+
+      peers = [
+        {
+          publicKey = "2eXVN6SgSjf6NrlSk111y5GBfTFD2VuW0eY6rBszfVc=";
+          #presharedKeyFile = "/root/wireguard-keys/preshared_from_peer0_key";
+          allowedIPs = [ "0.0.0.0/0" ];
+          endpoint = "mgn.daheim.site:51820";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
+  };
 }
